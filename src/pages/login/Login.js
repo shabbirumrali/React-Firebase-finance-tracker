4aed8b2e78d290e6e6f2c42ee6d2533c5594e99e
shabbirumrali <shabbirumrali@gmail.com>
2022-12-03 19:32:25 +0530

    added login form and styles for the login form

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,7 +1,41 @@
+import { useState } from 'react';
+
+// styles
+import styles from './Login.module.css';
 
 const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(email, password)
+  }
+
   return (
-    <div>Login</div>
+    <form onSubmit={handleSubmit} className={styles['login-form']}>
+      <h2>Login</h2>
+
+      <label>
+        <span>Email:</span>
+        <input 
+          type='email' 
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+        />
+      </label>
+
+      <label>
+        <span>Password:</span>
+        <input 
+          type='password'
+          onChange={e => setPassword(e.target.value)}
+          value={password} 
+        />
+      </label>
+
+      <button className="btn">Login</button>
+    </form>
   )
 }
 
