db8d2e7268b5a4637a1ca6c349b76c85b96edacc
shabbirumrali <shabbirumrali@gmail.com>
2022-12-05 17:34:49 +0530

    created auth Context hook, updated useSignup hook

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,9 +1,11 @@
 import { useState } from 'react'
+import { useAuthContext } from './useAuthContext'
 import { projectAuth } from '../firebase/config'
 
 export const useSignup = () => {
     const [error, setError] = useState(null)
     const [pending, setPending] = useState(false)
+    const { dispatch } = useAuthContext()
 
     const signup = async (email, password, name) => {
         setError(null)
@@ -18,7 +20,10 @@ export const useSignup = () => {
             }
             // update username
             await res.user.updateProfile({ displayName: name })
-            console.log('data submitted', res)
+            
+            // dispatch login action
+            dispatch({ type: 'LOGIN', payload: res.user })
+
             setError(null)
             setPending(false)
         } catch (err) {
