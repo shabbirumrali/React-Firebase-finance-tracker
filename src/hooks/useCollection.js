6f92c826a719f05c25d2d24b90ee26f3838bcca3
shabbirumrali <shabbirumrali@gmail.com>
2022-12-10 09:14:45 +0530

    transaction-form and transaction-list with custom hook

--- src/hooks/useCollection.js
+++ src/hooks/useCollection.js
@@ -1,26 +1,41 @@
-import { useState } from 'react'
-import { projectFirestore } from "../firebase/config"
+import { useState, useEffect, useRef } from 'react'
+import { projectFirestore } from '../firebase/config'
 
-export const useCollectoion = (collection) => {
-    const [isPending, setIsPending] = useState(false)
+export const useCollection = (collection, _query, _orderBy) => {
+    const [document, setDocument] = useState(null)
     const [error, setError] = useState(null)
- 
-    // collection ref
-    const ref = projectFirestore.collection(collection)
 
-    const getDocument = async (collectionName) => {
-        setIsPending(true)
-        setError(null)
+    const query = useRef(_query).current
+    const orderBy = useRef(_orderBy).current
 
-        try {
-            const unsubs = await ref.onSnapshot(() => {})
-            setIsPending(false)
-            setError(null)
-        } catch (err) {
-            console.log(err.message)
-            setError(err.message)
-            setIsPending(false)
+    useEffect(() => {
+        let ref = projectFirestore.collection(collection)
+
+        if(query) {
+            ref = ref.where(...query)
         }
-    }
-    return { isPending, error, getDocument }
+
+        if(orderBy) {
+            ref = ref.orderBy(...orderBy)
+        }
+
+        const unsubs = ref.onSnapshot((snapshot) => {
+            let result = []
+            snapshot.docs.forEach((doc) => {
+                result.push({ ...doc.data(), id: doc.id })
+            })
+
+            // updating the state
+            setDocument(result)
+            setError(null)
+        }, (error) => {
+            console.log(error)
+            setError('could not fetch the data')
+        })
+
+        // unsubscribe on unmount
+        return () => unsubs()
+    }, [collection, query, orderBy])
+
+    return { document, error }
 }
